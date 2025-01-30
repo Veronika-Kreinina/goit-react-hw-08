f87d9fa185cdd255923941762265815044106336
Veronika Kreinina <veronika.kr.0608@gmail.com>
2025-01-30 21:41:45 +0100

    add tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,13 @@ import { useDispatch, useSelector } from "react-redux";
 
 import { fetchContacts } from "./redux/contactsOps";
 import { selectError, selectIsLoading } from "./redux/selectors";
+import { Route, Routes } from "react-router-dom";
+import ContactsPage from "./pages/ContactsPage/ContactsPage";
+import HomePage from "./pages/HomePage/HomePage";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import RegistrationPage from "./pages/RegistrationPage/RegistrationPage";
+import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
+import Layout from "./components/Layout/Layout";
 function App() {
   const dispatch = useDispatch();
   const loading = useSelector(selectIsLoading);
@@ -17,16 +24,28 @@ function App() {
   }, [dispatch]);
 
   return (
-    <>
-      <div>
-        {loading && <p>Loading...</p>}
-        {error && <p>Ooops...Someting went wrong</p>}
-        <h1>Phonebook</h1>
-        <ContactForm />
-        <SearchBox />
-        <ContactList />
-      </div>
-    </>
+    <div>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="/contacts" element={<ContactsPage />} />
+        </Route>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/registration" element={<RegistrationPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </div>
+
+    // <>
+    //   <div>
+    //     {loading && <p>Loading...</p>}
+    //     {error && <p>Ooops...Someting went wrong</p>}
+    //     <h1>Phonebook</h1>
+    //     <ContactForm />
+    //     <SearchBox />
+    //     <ContactList />
+    //   </div>
+    // </>
   );
 }
 
---
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -0,0 +1,11 @@
+import ContactForm from "../../components/ContactForm/ContactForm";
+
+const ContactsPage = () => {
+  return (
+    <div>
+      <ContactForm />
+    </div>
+  );
+};
+
+export default ContactsPage;
